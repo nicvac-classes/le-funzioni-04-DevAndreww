d42fe979240f3ac04ea32eee279082897374f2e5
DevAndreww <andrea.sette2@marconibari.edu.it>
2025-12-03 16:22:56 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -1,11 +1,33 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+int input()
+{
+    int x;
+
+    do
+    {
+        cout << "Inserisci un numero positivo" << endl;
+
+        cin >> valore;
+        
+    }
+    while(x<=0);
+    
+}
+int main()
+{
+   int a,b;
+
+   int dichiarazA, dichiarazB, sommaA, sottrazioneA;
+
+   dichiarazA = input(a);
+
+   dichiarazB = input(b);
+
+   sommaA= somma(a,b);
+
+   sottrazioneA = sottrazione(a,b);
 }
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+
